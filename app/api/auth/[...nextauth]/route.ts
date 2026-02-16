8c3bf271c5da029047cacc49a9f21635221c1b82
Yash nirwan <yashnirwan18@gmail.com>
2026-02-16 22:41:10 +0530

    Google auth Initlize

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,35 @@
+import NextAuth from "next-auth";
+import GoogleProvider from 'next-auth/providers/google'
+
+const handler = NextAuth({
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID ?? "",
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
+            authorization: {
+                params: {
+                    prompt: "consent",
+                    access_type: "offline",
+                    response_type: "code",
+                    // Ask for Email Reading permissions
+                    scope: "openid email profile https://www.googleapis.com/auth/gmail.readonly",
+                },
+            },
+        }),
+    ],
+
+    callbacks: {
+        async session({ session, token }: any) {
+            session.accessToken = token.accessToken
+            return session
+        },
+        async jwt({ token, account}: any) {
+            if (account) {
+                token.accessToken = account.access_token
+            }
+            return token
+        }
+    }
+})
+
+export { handler as GET, handler as POST}
